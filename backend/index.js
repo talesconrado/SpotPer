8e032af6267ff11e452063c913f42f825f269f5c
Tales Conrado <talesmateuscc@gmail.com>
2019-11-17 17:16:02 -0300

    DONE: Musics' router

--- backend/index.js
+++ backend/index.js
@@ -7,8 +7,10 @@ const app = express();
 
 // routers
 const album = require('./router/album.js');
+const music = require('./router/music.js');
 
 app.use('/album', album);
+app.use('/music', music);
 
 const port = process.env.SERVER_PORT;
 const domain = (process.env.NODE_ENV === 'development') ? process.env.DB_DEV : process.env.DB_PROD;
