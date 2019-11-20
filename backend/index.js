b0cc6a494a1b016abfb6678419637e916fabb388
Phillipe Queiroz <phgqm7@gmail.com>
2019-11-19 23:44:52 -0300

    (DONE): Create playlist model.
    Create playlist mode. It takes values from database. Add playlist router. It deletes, and takes playlist from database. Removes music too.

--- backend/index.js
+++ backend/index.js
@@ -6,9 +6,15 @@ const express = require('express');
 const app = express();
 
 // routers
-const album = require('./router/album.js');
+const album = require('./router/album');
+const playlist = require('./router/playlist');
 
 app.use('/album', album);
+app.use('/playlist', playlist);
+
+app.use((req, res, next) => {
+	res.status(404).json({ status: 404, msg: `URL ${req.url} is not found` });
+});
 
 const port = process.env.SERVER_PORT;
 const domain = (process.env.NODE_ENV === 'development') ? process.env.DB_DEV : process.env.DB_PROD;
